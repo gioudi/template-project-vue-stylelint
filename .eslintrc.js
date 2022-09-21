87d1f8987aeeeb455d00957daa04bbd2fb2580e8
Semi-dios <tipouno@misena.edu.co>
2022-09-20 20:11:59 -0500

    feat/setting-eslint/add_eslint_rules

---
+++ .eslintrc.js
@@ -0,0 +1,64 @@
+module.exports = {
+    root: true,
+    env: {
+      node: true,
+      browser: true,
+    },
+    extends: ['plugin:vue/recommended', '@vue/airbnb'],
+    globals: {
+      sessionManager: true,
+    },
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+      'import/extensions': [
+        'error',
+        'always',
+        {
+          js: 'never',
+        },
+      ],
+      'max-len': [2, {
+        code: 120,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreUrls: true,
+        ignorePattern: 'd="([\\s\\S]*?)"',
+      }],
+      'array-bracket-newline': ['warn', { minItems: 3 }],
+      'array-element-newline': ['warn', { minItems: 3 }],
+      'vue/html-closing-bracket-newline': ['error', {
+        singleline: 'never',
+        multiline: 'never',
+      }],
+      'vue/max-attributes-per-line': ['error', {
+        singleline: 1,
+        multiline: {
+          max: 1,
+          allowFirstLine: false,
+        },
+      }],
+      'vue/component-name-in-template-casing': [
+        'error',
+        'kebab-case',
+        {
+          registeredComponentsOnly: true,
+          ignores: [],
+        },
+      ],
+      'no-param-reassign': ['error', {
+        props: true,
+        ignorePropertyModificationsFor: [
+          'state', 
+          'acc', 
+          'e', 
+        ],
+      }],
+      'linebreak-style': 0,
+    },
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+      parser: '@babel/eslint-parser',
+      sourceType: 'module',
+    },
+  };
